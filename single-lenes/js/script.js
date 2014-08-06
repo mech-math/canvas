4b03b4c6eb45b758cb1b80fa0a078fffebe90a8b
maksr51314 <maksr51314@gmail.com>
2014-08-06 20:37:33 +0300

    create single line

--- single-lenes/js/script.js
+++ single-lenes/js/script.js
@@ -5,31 +5,8 @@ $(document).ready(function() {
     canvas.width = $(window).width();
     canvas.height = $(window).height();
                     
-    var m_event = {clientX: 0, clientY: 0};
-    window.onmousemove = function(e) { m_event = e; };
-                    
-    frame = function(frame_c) {
-        canvas.width = canvas.width;
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        
-        drawCursor();
-        setTimeout(function() { frame(frame_c++); }, 10);
-    };              
-    frame();        
-                    
-    function drawCursor() {
-        ctx.beginPath();
-        ctx.strokeStyle = 'rgba(255, 255, 255, 1)';
-        ctx.moveTo(m_event.clientX - 10, m_event.clientY - 10);
-        ctx.lineTo(m_event.clientX + 10, m_event.clientY + 10);
-        ctx.moveTo(m_event.clientX + 10, m_event.clientY - 10);
-        ctx.lineTo(m_event.clientX - 10, m_event.clientY + 10);
-        ctx.stroke();
-        ctx.closePath();
-    }                
-    
-    window.onresize = function(e) {
-        canvas.width = $(window).width();
-        canvas.height = $(window).height();
-    };              
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(300,150);
+    ctx.stroke();
 });                 
